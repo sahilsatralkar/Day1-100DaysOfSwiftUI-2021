11dde18306ead5808c1caaa4bc48500df608a1b6
Sahil Satralkar <sahil.satralkar@gmail.com>
2021-11-13 20:57:50 +0530

    commit

--- Day1-100DaysOfSwiftUI.playground/Contents.swift
+++ Day1-100DaysOfSwiftUI.playground/Contents.swift
@@ -14,3 +14,42 @@ print(playerName)
 
 playerName = "Dani"
 print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Golden Retriever"
+let meaningOfLife = "How many roads must a man walk down?"
+
+var favouriteShow = "Breaking Bad"
+favouriteShow = "The Office"
+favouriteShow = "Better Call Saul"
+
+let actor = "Carrie Washington"
+let fileName = "paris.jpg"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+print(movie)
+
+print(movie.hasPrefix("A day"))
+
+var quote2 = "Change the world by being yourself"
+
+let score = 10
+let reallyBig = 10_00_000
+print(reallyBig)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let number2 = 0.1 + 0.2
+print(number2)
+
+let double1 = 3.1
+let double2 = 3131.31231
+let double3 = 3.0
+
