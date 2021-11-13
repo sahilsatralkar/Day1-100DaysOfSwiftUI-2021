4989a554d336f9ad5329a24932ccb40a39b2cd24
Sahil Satralkar <sahil.satralkar@gmail.com>
2021-11-13 19:41:06 +0530

    Initial Commit

---
+++ Day1-100DaysOfSwiftUI.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Sahil"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
